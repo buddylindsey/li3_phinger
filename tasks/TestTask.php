e0b132b571a9171d56c44c507b4333bdfece5bc2
Buddy Lindsey, Jr <buddy.lindsey@vitals.com>
2012-12-14 10:37:30 -0600

    adding basic formatting start

--- tasks/TestTask.php
+++ tasks/TestTask.php
@@ -1,32 +1,6 @@
 <?php
 
-/*
-This is free and unencumbered software released into the public domain.
-
-Anyone is free to copy, modify, publish, use, compile, sell, or
-distribute this software, either in source code form or as a compiled
-binary, for any purpose, commercial or non-commercial, and by any
-means.
-
-In jurisdictions that recognize copyright laws, the author or authors
-of this software dedicate any and all copyright interest in the
-software to the public domain. We make this dedication for the benefit
-of the public at large and to the detriment of our heirs and
-successors. We intend this dedication to be an overt act of
-relinquishment in perpetuity of all present and future rights to this
-software under copyright law.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
-OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-OTHER DEALINGS IN THE SOFTWARE.
-
-For more information, please refer to <http://unlicense.org/>
- */
-
+require 'XmlFormatter'
 
 /**
  * Lithium test task for Phing.
@@ -59,6 +33,20 @@ class TestTask extends Task
 	 * @var string
 	 */
 	protected $tests;
+
+	/**
+	 * Which formatter to use when ouputing results
+	 *
+	 * @var string
+	 */
+	protected $formatter;
+
+	/**
+	 * What location the all results output goes
+	 *
+	 * @var string
+	 */
+	protected $results;
 
 	/**
 	 * The main entry point for the task.
@@ -105,6 +93,10 @@ class TestTask extends Task
 		$result = $report->run();
 
 		$stats = $report->stats();
+
+		$formatter = new XmlFormatter($stats);
+		$formatter->save_output($results);
+
 		if (!$stats['success']) {
 			throw new BuildException("Unit tests failed for {$this->tests}. {$stats['count']['passes']}/{$stats['count']['asserts']} passes. Check lithium's test suite for more information.");
 		}
@@ -125,6 +117,16 @@ class TestTask extends Task
 		define('LITHIUM_LIBRARY_PATH', $li3Base . '/libraries');
 		define('LITHIUM_APP_PATH', $li3Base . '/app');
 		$this->li3Base = $li3Base;
+	}
+
+	public function getFormatter()
+	{
+		return $this->formatter;
+	}
+
+	public function getResults()
+	{
+		return $this->results;
 	}
 
 	public function getLi3Base()
